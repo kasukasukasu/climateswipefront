011afa3d0f9333d8aa491b9466f3dd1d3502b811
Johanna Yli-Sikkila <johanna.ylisikkilae@gmail.com>
2018-08-28 13:09:43 +0300

    RestFunctions after .gitignore

--- src/RestFunctions.js
+++ src/RestFunctions.js
@@ -11,3 +11,44 @@ export function fetchall(callback) {
             });
         });
 }
+
+export function fetchTask(id, callback) {
+    fetch(apiurli+'countries/'+id, {accept: 'application/json'})
+        .then(function(response) {
+            response.json().then(function(json) {
+                if (response.status >= 500)
+                    callback(json, response.status);
+                else
+                    callback(json);
+            });C
+        });
+}
+
+export function deleteCountry(id/*, callback*/) {
+    return fetch(apiurli+'countries/'+id, {method: 'DELETE'})
+        .then(function(response){
+            //callback();
+        })
+}
+
+export function createCountry(country, callback) {
+    fetch(apiurli+'countries/',  {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json' },
+        body: JSON.stringify(country)
+    })
+        .then((function(response) {
+            callback();
+        }));
+}
+
+export function updateCountry(country, callback) {
+    fetch(apiurli+'countries/'+country.id,  {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json' },
+        body: JSON.stringify(quote)
+    })
+        .then((function(response) {
+            callback();
+        }));
+}
