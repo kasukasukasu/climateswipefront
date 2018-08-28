19501e71b13b1781c6b6898cc3f09446d7cd956c
anninahe <anninaheiskanen@gmail.com>
2018-08-28 12:41:31 +0300

    Tyhjiä tiedostoja

---
+++ src/components/Navigation.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+
+export class Navigation extends Component {
+    render() {
+        return (
+            <Navbar staticTop>
+                <Navbar.Header>
+                    {/*<Navbar.Brand>*/}
+                        {/*<Link to="/">ClimateSwipe</Link>*/}
+                    {/*</Navbar.Brand>*/}
+                    <Navbar.Toggle/>
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem href="/taskslist">Ilmastoteot</NavItem>
+                        <NavItem href="/theswipe">The Swipe</NavItem>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavItem href="/login">Sisäänkirjautuminen</NavItem>
+                        <NavItem href="/signup">Rekisteröityminen</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
+
+export default Navigation;
