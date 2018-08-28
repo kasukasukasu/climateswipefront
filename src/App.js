e54f6f9ec304f4532c5e59c8aa6f2ed06592e61b
Johanna Yli-Sikkila <johanna.ylisikkilae@gmail.com>
2018-08-28 13:02:25 +0300

    RestFunctions

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Navigation from './components/Navigation';
 import SignUp from './components/SignUp';
-import TaskList from './components/TasksList';
+import TasksList from './components/TasksList';
 import TheSwipe from './components/TheSwipe';
 
 
@@ -16,11 +16,11 @@ const App = appProps => (
                     <Navigation/>
                     <Grid>
                         <Switch>
-                            <Route path="/Home" component={Home}/>
-                            <Route path="/Login" component={Login}/>
-                            <Route path="/SignUp" component={SignUp}/>
-                            <Route path="/TaskList" component={TaskList}/>
-                            <Route path="/TheSwipe" component={TheSwipe}/>
+                            <Route path="/home" component={Home}/>
+                            <Route path="/login" component={Login}/>
+                            <Route path="/signup" component={SignUp}/>
+                            <Route path="/taskslist" component={TasksList}/>
+                            <Route path="/theswipe" component={TheSwipe}/>
                         </Switch>
                     </Grid>
                 </div>
---
+++ src/RestFunctions.js
@@ -0,0 +1,13 @@
+const apiurli = 'api/';
+
+export function fetchall(callback) {
+    fetch(apiurli+'tasks', {accept: 'application/json'})
+        .then(function(response) {
+            response.json().then(function(json) {
+                if (response.status >= 500)
+                    callback(json, response.status);
+                else
+                    callback(json);
+            });
+        });
+}
