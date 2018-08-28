702d593358eaa09cff940d6d3e36cf8157ce1b7f
Johanna Yli-Sikkila <johanna.ylisikkilae@gmail.com>
2018-08-28 12:42:49 +0300

    App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Grid} from 'react-bootstrap';
 import './App.css';
 
+import Home from './components/Home';
+import Login from './components/Login';
+import Navigation from './components/Navigation';
+import SignUp from './components/SignUp';
+import TaskList from './components/TasksList';
+import TheSwipe from './components/TheSwipe';
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Home/>
+                    <Grid>
+                        <Switch>
+                            <Route path="/Home" component={Home}/>
+                            <Route path="/Login" component={Login}/>
+                            <Route path="/Navigation" component={Navigation}/>
+                            <Route path="/SignUp" component={SignUp}/>
+                            <Route path="/TaskList" component={TaskList}/>
+                            <Route path="/TheSwipe" component={TheSwipe}/>
+                        </Switch>
+                    </Grid>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
