20ca686ebd3dc4b1e6c7bbab3df8717ca9b3454c
kaarinasuom <kaarina.suominen@outlook.com>
2018-08-28 13:04:47 +0300

    toimiva firebase autentikaatio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// autentikoinnin tekeminen: yhdistetään react-app Firebase autentifikaatio moduuliin
+// mikä on Googlen Firebase?
+// Firebasen autentifikaatio moduuli hoitaa loginin ja kirjautumisen ja React-router hoitaa reitiyksen hallinnoinnin
+
+
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import PrivateRoute from "./PrivateRoute";
+import app from "./base";
+
+import Home from "./components/Home";
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    // tässä asetetaan alkuperäinen state
+    state = { loading: true, authenticated: false, user: null };
+
+    componentWillMount() {
+        app.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({
+                    authenticated: true,
+                    currentUser: user,
+                    loading: false
+                });
+            } else {
+                this.setState({
+                    authenticated: false,
+                    currentUser: null,
+                    loading: false
+                });
+            }
+        });
+    }
+
+    render() {
+        const { authenticated, loading } = this.state;
+
+        // renderöinti, kun haetaan autentikaatiota näytetään viestiä sivua ladataan
+
+        if (loading) {
+            return <p>Tietoja ladataan...</p>;
+        }
+
+        // sitten renderöitään reitit
+        // oteaan privateroute käyttöön, jos halutaan määritellä sivut, joille pääsee vain kirjautunut käyttäjä
+        return (
+            <Router>
+                <div>
+                    {/*<PrivateRoute exact path="/" component={Home} authenticated={authenticated}/>*/}
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/login" component={LogIn} />
+                    <Route exact path="/signup" component={SignUp} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/SignUp/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import SignUpView from "./SignUpView";
+import { withRouter } from "react-router";
+import app from "../../base";
+
+
+// luodaan rekisteröitymiscontainer komponentti index failiin
+
+class SignUpContainer extends Component {
+
+    // handleSignup funktio määritelty nuolifunktioksi, tehtiin tässä, jotta ei tarvitse käyttää bind(this)
+    // Tässä funktiossa preventDefault jotta vältetään sivun reloadaamista
+    // haetaan salasana ja sähköposti formielementiltä
+    // luodaan uusi käyttäjä firebaseen käyttäen  createUserWithEmailAndPassword funktiota
+    handleSignUp = async event => {
+        event.preventDefault();
+        const { email, password } = event.target.elements;
+        try {
+            const user = await app
+                .auth()
+                .createUserWithEmailAndPassword(email.value, password.value);
+            this.props.history.push("/");
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    render() {
+        return <SignUpView onSubmit={this.handleSignUp} />;
+    }
+}
+
+export default withRouter(SignUpContainer);
