41f6d4db5fa43c6d5a42cd1bedd928c8dc8372bb
kaarinasuom <kaarina.suominen@outlook.com>
2018-08-30 09:19:45 +0300

    buttoneja unohtui

--- src/components/SwipeButtons.js
+++ src/components/SwipeButtons.js
@@ -16,8 +16,6 @@ export class Buttons extends Component {
     render() {
         return (
             <div>
-                <button onClick={this.handleClick.bind(this, "EOukqsHJwsgodzgrSzbu8XpYPWa2", "1")}> Lisää listaan! </button>
-                <button onClick={this.handleClick.bind(this, "EOukqsHJwsgodzgrSzbu8XpYPWa2", "0" )}> Ehkä joskus myöhemmin... </button>
                 <button onClick={this.handleClick.bind(this, this.props.userid, "1")}> Lisää listaan! </button>
                 <button onClick={this.handleClick.bind(this, this.props.userid, "0" )}> Ehkä joskus myöhemmin... </button>
             </div>
