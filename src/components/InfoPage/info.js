5798dc941f28a0b53e6a95e471d0e3c9ba1c89f5
anninahe <anninaheiskanen@gmail.com>
2018-09-04 10:37:32 +0300

    info

--- src/components/InfoPage/info.js
+++ src/components/InfoPage/info.js
@@ -5,8 +5,9 @@ class Info extends Component {
     render() {
         return (
             <div className="info">
-            <h1 className="header-header"> Ilmastonmuutos - Aikamme suurin maailmanlaajuinen kriisi</h1><br/>
-                <p ><em>Ilmastonmuutos ihmisen toiminnan seurauksena on aikamme suurimpia kriisejä. Ilmastonmuutos itsessään ei ole uusi tai ennenkuulumaton tapahtuma, mutta sen nopeus ja suuruus ovat poikkeuksellisia. Ja kaikki tämä on ihmisen edesauttamaa. </em></p>
+            {/*<h1 className="header-header"> Ilmastonmuutos - Aikamme suurin maailmanlaajuinen kriisi</h1><br/>*/}
+                {/*<p ><em>Ilmastonmuutos ihmisen toiminnan seurauksena on aikamme suurimpia kriisejä. Ilmastonmuutos itsessään ei ole uusi tai ennenkuulumaton tapahtuma, mutta sen nopeus ja suuruus ovat poikkeuksellisia. Ja kaikki tämä on ihmisen edesauttamaa. </em></p>*/}
+                <p>Tulossa....</p>
             </div>
         )
     }
