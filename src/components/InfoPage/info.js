5fc1774c32f68ce44bafc072eda303be0936b1fb
kaarinasuom <kaarina.suominen@outlook.com>
2018-09-05 11:46:41 +0300

    onko toimii

--- src/components/InfoPage/info.js
+++ src/components/InfoPage/info.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Hiilijalanjalki from './hiilijalanjalki.png';
 
 
 class Info extends Component {
@@ -8,7 +9,8 @@ class Info extends Component {
             <h1 className="header-header"> Ilmastonmuutos - Aikamme suurin maailmanlaajuinen kriisi</h1><br/>
                 <p ><em>Ilmastonmuutos ihmisen toiminnan seurauksena on aikamme suurimpia kriisejä. Ilmastonmuutos itsessään ei ole uusi tai ennenkuulumaton tapahtuma, mutta sen nopeus ja suuruus ovat poikkeuksellisia. Ja kaikki tämä on ihmisen edesauttamaa. </em></p>
                 <p className="header-text">
-                    <h2>Tiesitko, että suomalaisten kulutustottumuksilla tarvitsisimme 3,4 maapalloa kattamaan valintojemme luonnonvarojen tarpeen? </h2><br/>
+                    <h2>Tiesitko, että suomalaisten kulutustottumuksilla tarvitsisimme 3,4 maapalloa kattamaan valintojemme luonnonvarojen tarpeen? </h2>
+                    <img className="botti" src={Hiilijalanjalki} circle width="750" height="400"/>
                     <p><em>Jos haluamme säästää luonnonvarojamme myös tuleville sukupolville, meidän tulisi pienentää kulutuksemme muodostamaa hiilijalanjälkeä huomattavasti. Jokainen kulutustamme pienentävä teko on lahja tuleville sukupolville ja maapallon hyvinvoinnille</em></p>
                 </p><br/>
             {/*<h1 className="header-header"> Ilmastonmuutos - Aikamme suurin maailmanlaajuinen kriisi</h1><br/>*/}
