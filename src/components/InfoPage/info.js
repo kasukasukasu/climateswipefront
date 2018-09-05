e475c7e07093a2f3d2dd50531fc6c2015a0f0891
anninahe <anninaheiskanen@gmail.com>
2018-09-05 13:32:18 +0300

    info

--- src/components/InfoPage/info.js
+++ src/components/InfoPage/info.js
@@ -7,13 +7,11 @@ class Info extends Component {
     render() {
         return (
             <div className="info">
-            <h1 className="header-header"> Ilmastonmuutos - Aikamme suurin maailmanlaajuinen kriisi</h1><br/>
-                <p ><em>Ilmastonmuutos ihmisen toiminnan seurauksena on aikamme suurimpia kriisejä. Ilmastonmuutos itsessään ei ole uusi tai ennenkuulumaton tapahtuma, mutta sen nopeus ja suuruus ovat poikkeuksellisia. Ja kaikki tämä on ihmisen edesauttamaa. </em></p>
-                <p className="header-text">
-                    <h2>Tiesitko, että suomalaisten kulutustottumuksilla tarvitsisimme 3,4 maapalloa kattamaan valintojemme luonnonvarojen tarpeen? </h2>
-                    <img className="botti" src={Hiilijalanjalki} circle width="750" height="400"/>
-                    <p><em>Jos haluamme säästää luonnonvarojamme myös tuleville sukupolville, meidän tulisi pienentää kulutuksemme muodostamaa hiilijalanjälkeä huomattavasti. Jokainen kulutustamme pienentävä teko on lahja tuleville sukupolville ja maapallon hyvinvoinnille</em></p>
-                </p><br/>
+            <h1 className="header-header"> Ilmastonmuutos ja hiilijalanjälki</h1>
+                <p className="header-text-1">Ilmastonmuutos ihmisen toiminnan seurauksena on aikamme suurimpia kriisejä. Ilmastonmuutos itsessään ei ole uusi tai ennenkuulumaton tapahtuma, mutta sen nopeus ja suuruus ovat poikkeuksellisia. Ja kaikki tämä on ihmisen edesauttamaa.</p>
+                    <p className="header-text-2">Tiesitko, että suomalaisten kulutustottumuksilla tarvitsisimme 3,4 maapalloa kattamaan valintojemme luonnonvarojen tarpeen? </p>
+                    <p className="header-text-3">Hiilijalanjälki on mittari, jota käytetään mittaamaan tuotteen toiminnan tai palvelun aiheuttamia kasvihuonekaasupäästöjä. Lisääntyvät kasvihuonekaasupäästöt nopeuttavat ilmastonlämpenemistä ja ovat suurin syy ilmastonmuutokseen.</p>
+                    <p className="header-text-3">Jos haluamme säästää luonnonvarojamme myös tuleville sukupolville, meidän tulisi pienentää kulutuksemme muodostamaa hiilijalanjälkeä huomattavasti. Jokainen kulutustamme pienentävä teko on lahja tuleville sukupolville ja maapallon hyvinvoinnille</p>
             {/*<h1 className="header-header"> Ilmastonmuutos - Aikamme suurin maailmanlaajuinen kriisi</h1><br/>*/}
                 {/*<p ><em>Ilmastonmuutos ihmisen toiminnan seurauksena on aikamme suurimpia kriisejä. Ilmastonmuutos itsessään ei ole uusi tai ennenkuulumaton tapahtuma, mutta sen nopeus ja suuruus ovat poikkeuksellisia. Ja kaikki tämä on ihmisen edesauttamaa. </em></p>*/}
             </div>
