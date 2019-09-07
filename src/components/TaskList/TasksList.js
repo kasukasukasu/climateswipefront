978b4c03b038ef9b97a09b54c2c2250ed0452187
kasukasukasu <kaarina.suominen@outlook.com>
2019-09-07 20:17:44 +0300

    added header to tasklist in error case

--- src/components/TaskList/TasksList.js
+++ src/components/TaskList/TasksList.js
@@ -63,8 +63,9 @@ class TasksList extends Component {
     } else {
                                   return (
                                           <div>
-                                                                 <br/>
-                                                                  <br/>
+                    <h2 className="taskcomponent">Esimerkkejä mahdollisista ilmastoteoista:</h2>
+                    <p className="header-text">Täältä löydät kätevästi kaikki ilmastoteot.<br/></p><br/>
+                    <input type="text" className="search" placeholder="Hae tekoja" onChange={this.searchHandler.bind(this)}/><br/>
                                                                      <br/>
                                                                         <div>
                                                                             <div className="container-fluid tausta-2">
